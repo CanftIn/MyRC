c0c37ae6387656b52d591cadcea327380b835183
CanftIn <wwc7033@gmail.com>
2019-04-05 23:49:26 +0800

    layer test

--- .emacs.d/private/CanftIn/packages.el
+++ .emacs.d/private/CanftIn/packages.el
@@ -30,33 +30,11 @@
 ;;; Code:
 
 (defconst CanftIn-packages
-  '()
-  "The list of Lisp packages required by the CanftIn layer.
+  '(
+    2048-game
+    ))
+  "The list of Lisp packages required by the CanftIn layer."
 
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
-
-
+(defun CanftIn/init-2048-game ()
+  (use-package 2048-game))
 ;;; packages.el ends here
